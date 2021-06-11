dea11042d9ec7ffff0b78492dc9cecefebe3585f
leyuanL <74730324+leyuanL@users.noreply.github.com>
2021-06-12 01:02:15 +0200

    改个小bug

--- site/templates/home.php
+++ site/templates/home.php
@@ -79,7 +79,7 @@
 
             if($happening->from()->toDate() <= time() && $happening->to()->toDate() >= time()) echo " "."now";
             if($happening->from()->toDate('Y-m-d') <= date('Y-m-d') && $happening->to()->toDate('Y-m-d') >= date('Y-m-d')) echo " "."today";
-            $isstart=$happening->from()->toDate() <= strtotime('+7 day')&& $happening->to()->toDate('Y-m-d') >= date('Y-m-d');
+            $isstart=$happening->from()->toDate() <= strtotime('+7 day')&& $happening->from()->toDate('Y-m-d') >= date('Y-m-d');
             $isend=$happening->to()->toDate() <= strtotime('+7 day');
             if($isstart||$isend)   echo " "."week";
           ?>
