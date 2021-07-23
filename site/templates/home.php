1bf9092a16367608f17211a8dd14078e8f905a09
Xinyi0114 <lixinyi.1203@gmail.com>
2021-07-24 00:47:57 +0200

    link2

--- site/templates/home.php
+++ site/templates/home.php
@@ -98,7 +98,7 @@
                 <span class="list-address"><?= $happening->street().$happening->zip()." ".$happening->district() ?></span>
               <span class="list-description"><?= $happening->description() ?></span>
               <span class="list-meta-info"><?= $happening->metaInfo() ?></span>
-              <a><span class="list-link"><?= $happening->link() ?></span></a>
+              <a class="list-link"><?= $happening->link() ?></a>
             </div>
           </div>
 
