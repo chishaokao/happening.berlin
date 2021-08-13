4be5656c50cf84afacf081fb33aa464f23ad000e
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-08-14 02:00:28 +0800

    link

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,7 +100,7 @@ function getComboFilter() {
     $(this)
       .addClass('current')
       $('.current .list-content').slideDown(1000)
-      $(this).click(function(){
+      $(this).children().filter('a')click(function(){
         $('.current .list-content').slideUp(1000)
         $(this).removeClass('current')
 
